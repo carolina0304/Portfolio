525f2bf6d4af14e860a489937d46bf96b006a19f
carolina0304 <garlene122@gmail.com>
2026-05-18 12:08:50 -0600

    Create react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+import "@fontsource/bebas-neue";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="page">{}</div>
+    </>
+  );
+}
+
+export default App;
